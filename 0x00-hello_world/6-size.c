e14f3225ec3aa2b075ceb772c0fd5865d87f1e42
Bahija18 <bahija.chlif@gmail.com>
2023-04-20 19:30:14 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
-* main - Entry 
-*
-* Return: Always 0
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-      printf("Size of a char: %i byte(s)\n",  sizeof(char));
-Repeat the same line but where you see char..u replace with an int, 
-Third line replace with...a long int, 
-Fourth line replace with...a long long int,
-Fifth line replace with...a float.
-}
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
