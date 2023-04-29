bea0e136f0cbef96dee5a177c38ba748916f9470
Bahija18 <bahija.chlif@gmail.com>
2023-04-28 18:40:26 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include 'main.h'
+#include <stdio.h>
+/**
+ *@c : char to check
+ * _isupper - uppercase characters
+ *
+ * return : 0 or 1
+ */
+int _isupper(int c)
+{
+if (c = 'A' & c = 'a')
+return (1);
+else
+if (c > 'A' & c > 'a')
+return (0);
+}
